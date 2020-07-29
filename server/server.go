d8037ddaee3d914f757d6d86bf4ae9e665d14016
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-07-30 02:04:57 +0545

    Added code comments

--- server/server.go
+++ server/server.go
@@ -12,29 +12,37 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "building-microservices", log.LstdFlags)
+
+	// create the handlers
 	newHello := handler.NewHello(l)
 	newBye := handler.NewBye(l)
 	newProduct := handler.NewProduct(l)
 
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/hello", newHello)
 	sm.Handle("/bye", newBye)
 	sm.Handle("/", newProduct)
 
+	// create a new server
 	s := &http.Server{
-		Addr:              ":9090",
-		Handler:           sm,
-		IdleTimeout:       120 * time.Second,
-		ReadHeaderTimeout: 1 * time.Second,
-		WriteTimeout:      1 * time.Second,
+		Addr:              ":9090",           // configure the bind address
+		Handler:           sm,                // set the default handler
+		ErrorLog:          l,                 // set the logger for the server
+		ReadHeaderTimeout: 1 * time.Second,   // max time to read request from the client
+		WriteTimeout:      1 * time.Second,   // max time to write response to the client
+		IdleTimeout:       120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Fatal(err)
 		}
 	}()
+
+	// trap sigterm or interrupt and gracefully shutdown the server
 	signalChannel := make(chan os.Signal)
 	signal.Notify(signalChannel, os.Interrupt)
 	signal.Notify(signalChannel, os.Kill)
