6f78b8c6d11691f0a7b8bbd2a43f55a57d9ad1d6
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-07-30 02:52:23 +0545

    Line comments replaced with block comments

--- handler/product.go
+++ handler/product.go
@@ -10,7 +10,7 @@ type Product struct {
 	l *log.Logger
 }
 
-func NewProduct(l *log.Logger) *Product  {
+func NewProduct(l *log.Logger) *Product {
 	return &Product{l}
 }
 
@@ -25,19 +25,21 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 func (p *Product) getProducts(rw http.ResponseWriter, r *http.Request) {
 	pl := data.GetProducts()
-	//// serializing into JSON with Marshaling
-	//d, err := json.Marshal(pl)
-	//if err != nil {
-	//	http.Error(rw, "Unable to marshal d into JSON.", http.StatusInternalServerError)
-	//}
-	//_, err = rw.Write(d)
-	//if err != nil {
-	//	http.Error(rw, "Unable to write d into JSON.", http.StatusInternalServerError)
-	//}
+	/*
+		One way of serializing structs into JSON is with Marshaling
+		d, err := json.Marshal(pl)
+		if err != nil {
+			http.Error(rw, "Unable to marshal d into JSON.", http.StatusInternalServerError)
+		}
+		_, err = rw.Write(d)
+		if err != nil {
+			http.Error(rw, "Unable to write d into JSON.", http.StatusInternalServerError)
+		}
 
-	// Marshaling and Encoder in a sense does same thing i.e writes the JSON encoding of v to the stream
-	// But rather than returning slice of data and error, encoder is writing the output directly to the io stream
-	// Why Encoder?: Don't have to buffer any Memory, don't have to allocate memory for that data object. Marginally Faster
+		Marshaling and Encoder in a sense does same thing i.e writes the JSON encoding of v to the stream
+		But rather than returning slice of data and error, encoder is writing the output directly to the io stream
+		Why Encoder?: Don't have to buffer any Memory, don't have to allocate memory for that data object. Marginally Faster
+	*/
 
 	err := pl.ToJSON(rw)
 	if err != nil {
